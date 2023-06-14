72aad620ad15aad3ff293e81c39860260d443fc3
Cesar Arnaez Flores <c_arnaez.flores@hotmail.com>
2023-06-14 14:35:59 -0400

    Deply Vercel

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,8 +1,8 @@
 
 export const Global = {
 
-    url: 'https://api-blog-production-88c5.up.railway.app'
-
+    url: 'https://api-blog-production-88c5.up.railway.app/api/'
+    // url: 'http://localhost:3900/api/'
 }
 
 
