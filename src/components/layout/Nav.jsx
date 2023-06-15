4af6ff12adca5b88b0bea012deeba3073bda94e1
Cesar Arnaez Flores <c_arnaez.flores@hotmail.com>
2023-06-15 19:30:01 -0400

    Ocultando link contacto y boton editar

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -10,7 +10,7 @@ export const Nav = () => {
         <li><NavLink to='/inicio'>Inicio</NavLink></li>
         <li><NavLink to='/articulos'>Articulos</NavLink></li>
         <li><NavLink to='/crear-articulos'>Crear articulos</NavLink></li>
-        <li><a href='#'>Contacto</a></li>
+        {/* <li><a href='#'>Contacto</a></li> */}
       </ul>
     </div>
   )
